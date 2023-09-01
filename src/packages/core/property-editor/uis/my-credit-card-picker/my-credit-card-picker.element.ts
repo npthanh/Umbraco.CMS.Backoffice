60af12feb5fe6d4fecffba95362a98bc90fafa9a
Niels Lyngsø <niels.lyngso@gmail.com>
2023-09-01 21:44:26 +0200

    more work

--- src/packages/core/property-editor/uis/my-credit-card-picker/my-credit-card-picker.element.ts
+++ src/packages/core/property-editor/uis/my-credit-card-picker/my-credit-card-picker.element.ts
@@ -1,6 +1,6 @@
 // TODO: Do not use Umb in the name here:
 import { UmbCreditCardElement } from './my-credit-card.element.js';
-import { html, customElement, property, css, LitElement } from '@umbraco-cms/backoffice/external/lit';
+import { html, customElement, property, state, css, LitElement, repeat } from '@umbraco-cms/backoffice/external/lit';
 import { UmbElementMixin } from '@umbraco-cms/backoffice/element-api';
 import { UmbPropertyEditorExtensionElement } from '@umbraco-cms/backoffice/extension-registry';
 import { UUITextStyles } from '@umbraco-cms/backoffice/external/uui';
@@ -8,6 +8,8 @@ import type { UmbDataTypeConfigCollection } from '@umbraco-cms/backoffice/compon
 
 import './my-credit-card.element.js';
 import { UMB_VARIANT_DATASET_CONTEXT } from '@umbraco-cms/backoffice/workspace';
+import { UmbVariantId } from '@umbraco-cms/backoffice/variant';
+import { tryExecuteAndNotify } from '@umbraco-cms/backoffice/resources';
 
 @customElement('umb-credit-card-picker')
 export class UmbCreditCardPickerElement extends UmbElementMixin(LitElement) implements UmbPropertyEditorExtensionElement {
@@ -18,12 +20,51 @@ export class UmbCreditCardPickerElement extends UmbElementMixin(LitElement) impl
 	@property({ attribute: false })
 	public config?: UmbDataTypeConfigCollection;
 
+	@state()
+	private _filteredCards:any[] = [];
+
+	#variantId: UmbVariantId | undefined;
+
+	#allCards = [
+		{
+			name: 'Visa',
+			value: 'visa',
+			cultures: ['en-us', 'da-dk']
+		},
+		{
+			name: 'Mastercard',
+			value: 'mastercard',
+			cultures: ['en-us', 'da-dk']
+		},
+		{
+			name: 'Dankort',
+			value: 'dankort',
+			cultures: ['da-dk']
+		},
+	];
+
 	constructor() {
 		super();
+
+
 		this.consumeContext(UMB_VARIANT_DATASET_CONTEXT, (context) => {
-			console.log("variant ID:", context.getVariantId());
+			this.#variantId = context.getVariantId();
+			this.filterCards();
 		});
+
+		/*
+		tryExecuteAndNotify(this, fetch(`/umbraco/management/api/v1/media/details/`)
+		.then((res) => res.json()))
+		*/
+
 	}
+
+
+	private filterCards() {
+		if(!this.#variantId || !this.#allCards) return;
+		this._filteredCards = this.#allCards.filter(card => card.cultures.includes(this.#variantId!.culture || 'invariant'));
+	}
+
 
 	private _onCardClicked = (e:MouseEvent) => {
 		const target = e.target as UmbCreditCardElement;
@@ -32,12 +73,15 @@ export class UmbCreditCardPickerElement extends UmbElementMixin(LitElement) impl
 	};
 
 	render() {
-		return html`
-			<my-credit-card @selected=${this._onCardClicked} name="Visa" value="visa" ?selected=${"visa" === this.value}>
-			</my-credit-card>
-			<my-credit-card @selected=${this._onCardClicked} name="Mastercard" value="mastercard" ?selected=${"mastercard" === this.value}>
-			</my-credit-card>
-		`;
+		console.log("render", this._filteredCards)
+		return repeat(this._filteredCards,
+			(card) => card.value,
+			(card) =>
+				html`
+					<my-credit-card @selected=${this._onCardClicked} name=${card.name} value=${card.value} ?selected=${card.value === this.value}>
+					</my-credit-card>
+				`
+			);
 	}
 
 	static styles = [UUITextStyles, css`
