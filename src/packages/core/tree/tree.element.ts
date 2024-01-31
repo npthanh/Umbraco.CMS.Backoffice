54bb1b89ee03d6294e85fb7fdd5a109897eac180
Mads Rasmussen <madsr@hey.com>
2024-01-31 14:49:55 +0100

    clean up

--- src/packages/core/tree/tree.element.ts
+++ src/packages/core/tree/tree.element.ts
@@ -2,11 +2,7 @@ import { customElement, html, property, state } from '@umbraco-cms/backoffice/ex
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 import type { ManifestTree } from '@umbraco-cms/backoffice/extension-registry';
 import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
-import {
-	UmbExtensionElementInitializer,
-	createExtensionApi,
-	createExtensionElement,
-} from '@umbraco-cms/backoffice/extension-api';
+import { UmbExtensionElementInitializer, createExtensionApi } from '@umbraco-cms/backoffice/extension-api';
 
 @customElement('umb-tree')
 export class UmbTreeElement extends UmbLitElement {
@@ -26,16 +22,14 @@ export class UmbTreeElement extends UmbLitElement {
 	}
 	set props(newVal: Record<string, unknown> | undefined) {
 		// TODO, compare changes since last time. only reset the ones that changed. This might be better done by the controller is self:
-		debugger;
 		this.#props = newVal;
-		if (this.#extensionsController) {
-			this.#extensionsController.properties = newVal;
+		if (this.#extensionElementController) {
+			this.#extensionElementController.properties = newVal;
 		}
-		this.#observeManifest();
 	}
 	#props?: Record<string, unknown> = {};
 
-	#extensionsController?: UmbExtensionElementInitializer<ManifestTree>;
+	#extensionElementController?: UmbExtensionElementInitializer<ManifestTree>;
 
 	@state()
 	_element: HTMLElement | undefined;
@@ -43,19 +37,17 @@ export class UmbTreeElement extends UmbLitElement {
 	#manifest?: ManifestTree;
 
 	#observeManifest() {
-		debugger;
-		if (this._alias && this.props) {
-			this.observe(
-				umbExtensionsRegistry.byTypeAndAlias('tree', this._alias),
-				async (manifest) => {
-					if (!manifest) return;
-					this.#manifest = manifest;
-					this.#createApi();
-					this.#createElement();
-				},
-				'umbObserveTreeManifest',
-			);
-		}
+		if (!this._alias) return;
+		this.observe(
+			umbExtensionsRegistry.byTypeAndAlias('tree', this._alias),
+			async (manifest) => {
+				if (!manifest) return;
+				this.#manifest = manifest;
+				this.#createApi();
+				this.#createElement();
+			},
+			'umbObserveTreeManifest',
+		);
 	}
 
 	async #createApi() {
@@ -68,35 +60,20 @@ export class UmbTreeElement extends UmbLitElement {
 	async #createElement() {
 		if (!this.#manifest) throw new Error('No manifest');
 
-		this.#createController();
-
-		//this._element = await createExtensionElement(this.#manifest);
-		//this.requestUpdate();
-	}
-
-	#createController() {
-		debugger;
-
-		if (!this.#manifest) throw new Error('No manifest');
-
 		const extController = new UmbExtensionElementInitializer<ManifestTree>(
 			this,
 			umbExtensionsRegistry,
 			this.#manifest.alias,
 			this.#extensionChanged,
 			'umb-tree-default',
-		) as any;
+		);
 
 		extController.properties = this.#props;
 
-		this.#extensionsController = extController;
-
-		//return extController;
+		this.#extensionElementController = extController;
 	}
 
 	#extensionChanged = (isPermitted: boolean, controller: UmbExtensionElementInitializer<ManifestTree>) => {
-		console.log('LK.extensionChanged', [isPermitted, controller]);
-		debugger;
 		this._element = controller.component;
 		this.requestUpdate('_element');
 	};
